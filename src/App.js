b01445c542361074210ffc739c513e5a759dbc70
Sushant Sharma <itsofficialsushant@gmail.com>
2025-10-31 21:28:41 +0100

    Form switching Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AllowanceForm from './appeal/AllowanceForm';
+import RelationshipVerification from './english-format/RelationshipVerification';
 import './App.css';
 
 function App() {
+  const [form, setForm] = useState('allowance');
+
+  const renderForm = () => {
+    switch (form) {
+      case 'allowance':
+        return <AllowanceForm />;
+      case 'relationship':
+        return <RelationshipVerification />;
+      default:
+        return <AllowanceForm />;
+    }
+  };
+
   return (
     <div className="App">
-      <AllowanceForm />
+      <select onChange={(e) => setForm(e.target.value)} value={form}>
+        <option value="allowance">Allowance Form</option>
+        <option value="relationship">Relationship Verification</option>
+      </select>
+
+      {renderForm()}
     </div>
   );
 }
